809cf69d4d3f5e438a2e71a0ebf0596d2b7e058c
callytat <104803387+callytat@users.noreply.github.com>
2022-06-15 20:16:56 -0700

    Converted the units into imperial

--- src/index.js
+++ src/index.js
@@ -100,7 +100,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "8440d16dc7b02d6a7589879556b1eead";
-  let units = "metric";
+  let units = "imperial";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -150,7 +150,6 @@ function searchLocation(position) {
   let units = "imperial";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showTemperature);
-  console.log(apiUrl);
 }
 
 function getCurrentLocation(event) {
